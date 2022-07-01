dc0dfba2dbbf310f6a38e77b15da1eb737bea6a7
HarishKotturi <nimbus.harish@gmail.com>
2022-07-01 20:27:33 +0530

    Intial commit

---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbTabsetModule,
+  NbUserModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbListModule,
+  NbIconModule,
+} from '@nebular/theme';
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { DashboardComponent } from './dashboard.component';
+import { StatusCardComponent } from './status-card/status-card.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { RoomSelectorComponent } from './rooms/room-selector/room-selector.component';
+import { PlayerComponent } from './rooms/player/player.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    ThemeModule,
+    NbCardModule,
+    NbUserModule,
+    NbButtonModule,
+    NbTabsetModule,
+    NbActionsModule,
+    NbRadioModule,
+    NbSelectModule,
+    NbListModule,
+    NbIconModule,
+    NbButtonModule,
+    NgxEchartsModule,
+  ],
+  declarations: [
+    DashboardComponent,
+    StatusCardComponent,    
+    ContactsComponent,
+    RoomSelectorComponent,    
+    RoomsComponent,    
+    PlayerComponent       
+  ],
+})
+export class DashboardModule { }
